a4a1b3d15223bf16a13f31a51b47120ca8b6a287
Vadym S. Khondar <v.khondar@invisilabs.com>
2015-11-07 12:47:26 +0200

    Extend usecase to multiple features registered in cluster.

--- src/main/java/poc/cluster/ClusterApp.java
+++ src/main/java/poc/cluster/ClusterApp.java
@@ -15,15 +15,15 @@ public class ClusterApp {
     public static void main(String[] args) {
         final ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(ClusterConfig.class);
 
-        final Runnable worker = new Runnable() {
+        Runnable worker = new Runnable() {
             @Override
             public void run() {
                 while (!Thread.currentThread().isInterrupted()) {
-                    System.out.println("Worker is running");
+                    System.out.println(Thread.currentThread().getName() + " Worker is running");
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
-                        logger.warn("Worker interrupted");
+                        logger.warn(Thread.currentThread().getName() + "Worker interrupted");
                         break;
                     }
                 }
@@ -31,35 +31,13 @@ public class ClusterApp {
         };
 
         final HighAvailabilityRegistry<HazelcastClusterMember, String> registry = (HighAvailabilityRegistry<HazelcastClusterMember, String>) ctx.getBean("haRegistry");
-        registry.joinFeatureCluster("poc.cluster.ha", new FeatureAvailabilityListener<String, HazelcastClusterMember>() {
+        registry.joinFeatureCluster("poc.cluster.ha0", new FeatureAvailabilityListenerImpl("poc.cluster.ha0", worker));
 
-            private Thread workerThread = null;
-
-            @Override
-            public void onMasterRoleAssigned(String s) {
-                if (workerThread != null) {
-                    logger.error("Worker is not null when this node becomes master");
-                } else {
-                    workerThread = new Thread(worker);
-                    workerThread.start();
-                    logger.info("Started worker as this node becomes master");
-                }
-            }
-
-            @Override
-            public void onMasterChange(String s, HazelcastClusterMember self, HazelcastClusterMember oldMaster, HazelcastClusterMember newMaster) {
-                logger.info("Feature '" + s + "' changes master from " + oldMaster + " to " + newMaster);
-            }
-
-            @Override
-            public void onMasterRoleUnassigned(String s) {
-                if (workerThread != null) {
-                    workerThread.interrupt();
-                    workerThread = null;
-                    logger.info("Stopped worker as this node is no longer master");
-                }
-            }
-        });
+        if (registry.getLocalMember().hashCode() % 2 == 0) {
+            registry.joinFeatureCluster("poc.cluster.ha1", new FeatureAvailabilityListenerImpl("poc.cluster.ha1", worker));
+        } else {
+            registry.joinFeatureCluster("poc.cluster.ha2", new FeatureAvailabilityListenerImpl("poc.cluster.ha2", worker));
+        }
 
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
@@ -68,5 +46,44 @@ public class ClusterApp {
                 logger.info("Instance shut down");
             }
         });
+    }
+}
+
+class FeatureAvailabilityListenerImpl implements FeatureAvailabilityListener<String, HazelcastClusterMember> {
+
+    private final static Logger logger = LoggerFactory.getLogger(FeatureAvailabilityListener.class);
+
+    private String feature;
+    private Thread workerThread = null;
+    private Runnable worker;
+
+    public FeatureAvailabilityListenerImpl(String feature, Runnable worker) {
+        this.feature = feature;
+        this.worker = worker;
+    }
+
+    @Override
+    public void onMasterRoleAssigned(String s) {
+        if (workerThread != null) {
+            logger.error(feature + ": Worker is not null when this node becomes master");
+        } else {
+            workerThread = new Thread(worker, feature);
+            workerThread.start();
+            logger.info(feature + ": Started worker as this node becomes master");
+        }
+    }
+
+    @Override
+    public void onMasterChange(String s, HazelcastClusterMember self, HazelcastClusterMember oldMaster, HazelcastClusterMember newMaster) {
+        logger.info(feature + ": Feature changes master from " + oldMaster + " to " + newMaster);
+    }
+
+    @Override
+    public void onMasterRoleUnassigned(String s) {
+        if (workerThread != null) {
+            workerThread.interrupt();
+            workerThread = null;
+            logger.info(feature + ": Stopped worker as this node is no longer master");
+        }
     }
 }
